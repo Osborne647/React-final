9ec94ffd733ccf61fdafd1452a5a8f7eb730033b
Osborne647 <osborne647@gmail.com>
2026-07-08 13:51:21 -0500

    app commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+import Nav from "./components/nav.jsx"
+import LandingPage from "./components/landingpage.jsx"
+import Games from "./pages/games.jsx"
+import GameDetail from './pages/gamedetail.jsx';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom' 
+import { useNavigate } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/games" element={<Games />} />
+          <Route path="/games/:id" element={<GameDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App
